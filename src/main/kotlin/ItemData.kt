ba2b50847db639e207974710b2506dfc43dcc2cd
emiliosolanoo21 <85262580+emiliosolanoo21@users.noreply.github.com>
2023-07-16 23:16:39 -0600

    ItemData

--- src/main/kotlin/ItemData.kt
+++ src/main/kotlin/ItemData.kt
@@ -1,3 +1,25 @@
-class ItemData {
+class ItemData(val originalValue: Any?) {
+    fun getType(): String? {
+        return when (originalValue) {
+            is String -> "cadena"
+            is Int -> "entero"
+            is Boolean -> "booleano"
+            else -> null
+        }
+    }
 
+    fun getInfo(): String? {
+        return when (originalValue) {
+            is String -> "L${originalValue.length}"
+            is Int -> when {
+                originalValue % 10 == 0 -> "M10"
+                originalValue % 5 == 0 -> "M5"
+                originalValue % 2 == 0 -> "M2"
+                else -> null
+            }
+            is Boolean -> if (originalValue) "Verdadero"
+                else "Falso"
+            else -> null
+        }
+    }
 }
