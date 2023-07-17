81b83c5c9acff8d78faaeb44f5332dc03703931e
emiliosolanoo21 <85262580+emiliosolanoo21@users.noreply.github.com>
2023-07-16 23:47:16 -0600

    Comentarios ItemData

--- src/main/kotlin/ItemData.kt
+++ src/main/kotlin/ItemData.kt
@@ -1,4 +1,16 @@
+/*
+Guatemala, julio 16 de 2023
+Universidad del Valle de Guatemala
+Programacion de plataformas moviles, seccion 10
+Emilio Jose Solano Orozco, carne 21212
+
+Lab. 1
+ItemData.kt
+ */
+
 class ItemData(val originalValue: Any?) {
+
+    //En menos espacio que en Java, se realiza la funcion getType gracias al When Statement.
     fun getType(): String? {
         return when (originalValue) {
             is String -> "cadena"
@@ -8,9 +20,15 @@ class ItemData(val originalValue: Any?) {
         }
     }
 
+    //En menos espacio que en Java, se realiza la funcion getInfo gracias al When Statement.
     fun getInfo(): String? {
         return when (originalValue) {
             is String -> "L${originalValue.length}"
+
+            /*
+            When dentro del When para delimitar las condiciones y conocer los multiplos y
+            los strings a devolver segun el caso.
+             */
             is Int -> when {
                 originalValue % 10 == 0 -> "M10"
                 originalValue % 5 == 0 -> "M5"
